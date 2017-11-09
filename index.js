7f26555c0496e34e85fa965c7728f5c986af1dcf
celiagomezdev <celiagomezdev@gmail.com>
2017-11-09 11:02:27 +0100

    Implement post method

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const BarService = require('./services/bar-service')
 
 const app = express()
 
 app.set('view engine', 'pug')
 
+app.use(bodyParser.json())
+
 app.get('/', async (req, res, next) => {
   res.render('index')
-  // res.sendFile(__dirname + '/index.html')
-  // res.send(await BarService.findAll())
 })
 
 app.get('/bar/all', async (req, res, next) => {
   const bars = await BarService.findAll()
 
   res.render('bar', { bars })
+})
+
+app.post('/bar', async (req, res, next) => {
+  const bar = await BarService.add(req.body)
+  console.log(bar)
+  res.send(bar)
 })
 
 app.listen(3000, () => {
