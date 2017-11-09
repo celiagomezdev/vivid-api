51b9af5f7bbb0c84c13fc04d14cf523c87db6208
celiagomezdev <celiagomezdev@gmail.com>
2017-11-09 10:16:01 +0100

    Prepare bar pug style

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const app = express()
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + '/index.html')
+app.get('/', async (req, res, next) => {
+  res.render('index')
+  // res.sendFile(__dirname + '/index.html')
   // res.send(await BarService.findAll())
+})
+
+app.get('/bar/all', async (req, res, next) => {
+  const bars = await BarService.findAll()
+
+  res.render('bar', { bars })
 })
 
 app.listen(3000, () => {
