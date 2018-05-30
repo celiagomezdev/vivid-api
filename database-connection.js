d918dd502e543363a878ce3b7f9a789428af6df0
celiagomezdev <celiagomezdev@gmail.com>
2018-05-30 17:43:50 +0200

    Update database connection data

--- database-connection.js
+++ database-connection.js
@@ -3,12 +3,6 @@ const config = require('config')
 
 mongoose.Promise = global.Promise
 
-<<<<<<< HEAD
 const connectionString = process.env.DB_URL || config.get('mongoProdUrl')
 
 mongoose.connect(connectionString, { useMongoClient: true })
-||||||| merged common ancestors
-mongoose.connect(config.get('mongoProdUrl'), { useMongoClient: true })
-=======
-mongoose.connect(config.get('mongoDevUrl'), { useMongoClient: true })
->>>>>>> master
